ac895d5b7e5ff5ac3154d34fd327a0a80536a6ad
Carlosbravo16 <110642356+Carlosbravo16@users.noreply.github.com>
2024-02-04 18:03:06 -0500

    se creo nuevo componente

--- src/app/navbar/navbar-module.module.ts
+++ src/app/navbar/navbar-module.module.ts
@@ -2,17 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar.component';
 import { AngularMaterialModule } from '../angular-material.module';
-import { CampaignsComponent } from './pages/campaigns/campaigns.component';
-import { ProvidersComponent } from './pages/providers/providers.component';
-import { CustomersComponent } from './pages/customers/customers.component';
-import { UsersComponent } from './pages/users/list-user/users.component';
-import { TipodocumentoComponent } from './pages/tipodocumento/manage-tipodocumento/tipodocumento.component';
 import { MenuRoutingRoutes } from './menu-routing.routing';
 
 
 @NgModule({
   declarations: [
     NavbarComponent
+  
   ],
   imports: [
     CommonModule,

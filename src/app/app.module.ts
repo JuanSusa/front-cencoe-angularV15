9b69a1ce2d870c471d65ad882fe4576130c7e0d8
DavidM0208 <sdcifuentes85@misena.edu.co>
2024-02-04 23:32:51 -0500

    Solucion de conflictos y actualizacion de ramas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+// import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -6,9 +6,7 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavbarModuleModule } from './navbar/navbar-module.module';
 import { CampaignsComponent } from './navbar/pages/campaigns/campaigns.component';
-
-
-
+import { NgModule } from '@angular/core';
 
 @NgModule({
   declarations: [

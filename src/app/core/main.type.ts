e79b360d04c4b569e4b350ea9f5e7bcd42f168de
sxroaaa <laura.roa3@misena.edu.co>
2024-02-24 20:07:17 -0500

    actualizacion modulo proveedores

--- src/app/core/main.type.ts
+++ src/app/core/main.type.ts
@@ -15,6 +15,16 @@ export interface ReqResponse<T> {
     success: boolean;
 }
 
+// Modulo proveedores
+export interface Provider {
+    providerId?: number | null;
+    providerName: String;
+    providerAddress: String;
+    providerEmail: String; 
+    providerContact: String;
+    providerDetails: String;
+    providerDoctype: TypeDocs;  
+}
 
 // Modulo usuario
 export interface User {
@@ -62,14 +72,3 @@ export interface Customer {
     customerState: boolean;
 }
 
-// Modulo proveedores
-export interface Provider {
-    providerId: number;
-    providerName: String;
-    providerAddress: String;
-    providerEmail: String; //ya esta en la bd
-    providerContact: String;//ya esta en la bd
-    providerDoctype: TypeDocs;  //ya esta en la bd
-    providerDetails: String;
-    providerState: boolean;
-}

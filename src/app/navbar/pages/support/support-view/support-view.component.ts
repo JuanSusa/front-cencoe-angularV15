1916fe7153f12c2f09146687d2617ca876d3c847
DavidM0208 <sdcifuentes85@misena.edu.co>
2024-02-29 18:17:39 -0500

    Se modifica el modulo de soporte

--- src/app/navbar/pages/support/support-view/support-view.component.ts
+++ src/app/navbar/pages/support/support-view/support-view.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./support-view.component.scss']
 })
 export class SupportComponent {
-
+  onFileSelected(event: any) {
+    const files: FileList = event.target.files;
+    if (files.length > 0) {
+      const file: File = files[0]; 
+      console.log('archivo seleccionado', file)
+    }
+  }
 }

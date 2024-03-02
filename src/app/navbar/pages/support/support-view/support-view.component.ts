924d5a1f926354d084f809d5d84d2b4344acf567
DavidM0208 <sdcifuentes85@misena.edu.co>
2024-03-02 17:06:59 -0500

    Se modifica el modulo de soporte, añadiendo descripción y cambiando tamaños del mismo

--- src/app/navbar/pages/support/support-view/support-view.component.ts
+++ src/app/navbar/pages/support/support-view/support-view.component.ts
@@ -6,11 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./support-view.component.scss']
 })
 export class SupportComponent {
-  onFileSelected(event: any) {
-    const files: FileList = event.target.files;
-    if (files.length > 0) {
-      const file: File = files[0]; 
-      console.log('archivo seleccionado', file)
-    }
-  }
+
 }
